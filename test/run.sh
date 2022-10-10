b62a0bcf8c3bdbadc3cdb7e2a0d39d0755c02515
jaywarfield-deveoper <jaywarfield.deveoper@gmail.com>
2022-10-10 08:54:56 -0400

    Renamed example.

--- test/run.sh
+++ test/run.sh
@@ -9,5 +9,5 @@ then
       python3 ../drawit.py -mode batch -region us-south -key $1
    fi
 else
-   python3 ../drawit.py -mode gui -input drawit.json -output downloads
+   python3 ../drawit.py -mode gui -input ibmvpc.json -output downloads
 fi
