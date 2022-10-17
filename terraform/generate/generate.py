69f5ba26c71e2b8e15396f54dbd66f83937d75ee
jaywarfield-deveoper <jaywarfield.deveoper@gmail.com>
2022-10-17 10:30:39 -0400

    Use standard copyright.

--- terraform/generate/generate.py
+++ terraform/generate/generate.py
@@ -1,6 +1,6 @@
 # @file generate.py
 #
-# Copyright IBM Corporation 2022
+# Copyright contributors to the buildIT project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
