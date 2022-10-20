66e21610287ec8ee952bf5c5201304ecad6498b3
jaywarfield-deveoper <jaywarfield.deveoper@gmail.com>
2022-10-20 17:16:24 -0400

    Add subnet and ip to vpes.

--- common/common.py
+++ common/common.py
@@ -20,7 +20,7 @@ from common.messages import Messages
 
 class Common:
    toolName = 'drawIT'
-   toolVersion = '0.5.31'
+   toolVersion = '0.5.32'
    toolTitle = toolName + ' ' + toolVersion
 
    options = None
